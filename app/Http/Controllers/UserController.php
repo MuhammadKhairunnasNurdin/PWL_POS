a1787c7806b18e6bd954d92fd552ec3e66d6b0a6
MuhammadKhairunnasNurdin <anasnurdin936@gmail.com>
2024-03-07 22:43:58 +0700

    feat(Retrieve Single Model): Praktikum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,18 +39,44 @@ class UserController extends Controller
          * Example mass assignment using Eloquent ORM (with attribute
          * $fillable)
          */
-        $data = [
+       /* $data = [
             'level_id' => 2,
             'username' => 'manager_tiga',
             'nama' => 'Manager 3',
             'password' => Hash::make('12345')
         ];
-        UserModel::create($data);
+        UserModel::create($data);*/
 
         /**
-         * Try access UserModel
+         * Try access all row in UserModel table
          */
-        $user = UserModel::all();
+        /*$user = UserModel::all();*/
+
+        /**
+         * finding row in UserModel table that has an id
+         */
+        /*$user = UserModel::find(1);*/
+
+        /**
+         * finding row in UserModel table with criteria(where()) and first
+         * row from that criteria
+         */
+        /*$user = UserModel::where('level_id', 1)->first();*/
+
+        /**
+         * this method has exact meaning with our above code
+         */
+        /*$user = UserModel::firstWhere('level_id', 1);*/
+
+        /**
+         * method for finding row with primary key, and different with
+         * find() method above, findOr() can handle if finding failed with
+         * error message like bellow or other way
+         */
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
