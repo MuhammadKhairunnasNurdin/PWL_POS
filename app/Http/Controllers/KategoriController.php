b044af3a58240378df5a1e6bdd3126765db9c4be
MuhammadKhairunnasNurdin <anasnurdin936@gmail.com>
2024-03-23 00:47:46 +0700

    feat(JS-6_Praktikum-B): Server Validation

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\DataTables\KategoriDataTable;
 use App\Models\KategoriModel;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
 
 class KategoriController extends Controller
 {
@@ -39,17 +41,21 @@ class KategoriController extends Controller
         return $dataTable->render('kategori.index');
     }
 
-    public function create()
+    public function create(): View
     {
         return view('kategori.create');
     }
 
-    public function store(Request $request)
+    public function store(Request $request):RedirectResponse
     {
-        KategoriModel::create([
-            'kategori_kode' => $request->kodeKategori,
-            'kategori_nama' => $request->namaKategori
+        $validated = $request->validate([
+            'kategori_kode' => 'bail|unique:m_kategori|required',
+            'kategori_nama' => 'bail|required|max:255'
         ]);
+
+        /**
+         * The post is valid...
+         */
         return redirect('/kategori');
     }
 
--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -15,18 +15,32 @@
         <form method="post" action="../kategori">
             <div class="card-body">
                 <div class="form-group">
-                    <label for="kodeKategori">Kode Kategori</label>
-                    <input type="text" class="form-control" id="kodeKategori" name="kodeKategori" placeholder="">
+                    <label for="kode_kategori">Kode Kategori</label>
+                    <input type="text" id="kode_kategori" name="kategori_kode" class="form-control @error('kategori_kode') is-invalid @enderror">
+
+                    @error('kategori_kode')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="namaKategori">Nama Kategori</label>
-                    <input type="text" class="form-control" id="namaKategori" name="namaKategori" placeholder="">
+                    <input type="text" class="form-control" id="namaKategori" name="kategori_nama" placeholder="">
                 </div>
 
                 <div class="card-footer">
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </div>
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </form>
+
     </div>
 @endsection
